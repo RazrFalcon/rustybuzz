2096ac576d55b1c230be48df08bffdaab1331e79
glowcoil <micahrjohnston@gmail.com>
2020-12-26 00:58:53 -0600

    Add GlyphInfo::unsafe_to_break
    Closes #27

--- src/buffer.rs
+++ src/buffer.rs
@@ -105,6 +105,20 @@ unsafe impl bytemuck::Pod for GlyphInfo {}
 const IS_LIG_BASE: u8 = 0x10;
 
 impl GlyphInfo {
+    /// Indicates that if input text is broken at the beginning of the cluster this glyph
+    /// is part of, then both sides need to be re-shaped, as the result might be different.
+    ///
+    /// On the flip side, it means that when this flag is not present,
+    /// then it's safe to break the glyph-run at the beginning of this cluster,
+    /// and the two sides represent the exact same result one would get if breaking input text
+    /// at the beginning of this cluster and shaping the two sides separately.
+    /// This can be used to optimize paragraph layout, by avoiding re-shaping of each line
+    /// after line-breaking, or limiting the reshaping to a small piece around
+    /// the breaking point only.
+    pub fn unsafe_to_break(&self) -> bool {
+        self.mask & glyph_flag::UNSAFE_TO_BREAK != 0
+    }
+
     #[inline]
     pub(crate) fn as_char(&self) -> char {
         char::try_from(self.codepoint).unwrap()
