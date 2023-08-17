12aeeec1b0598d2c9931729f55067bec9d6ac147
+merlan #flirora <uruwi@protonmail.com>
2023-08-17 04:56:42 -0400

    Buffer::serialize: surround serialized form with brackets
    This matches the output format used in HarfBuzz 2.7.4.

--- src/buffer.rs
+++ src/buffer.rs
@@ -1699,6 +1699,7 @@ impl GlyphBuffer {
         use core::fmt::Write;
 
         let mut s = String::with_capacity(64);
+        s.push('[');
 
         let info = self.glyph_infos();
         let pos = self.glyph_positions();
@@ -1754,6 +1755,7 @@ impl GlyphBuffer {
         if !s.is_empty() {
             s.pop();
         }
+        s.push(']');
 
         Ok(s)
     }
