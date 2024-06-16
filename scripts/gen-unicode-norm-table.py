78a549d8e6698f5b0968b8b25b8e781d5e41d50e
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2024-06-16 18:01:03 +0200

    Update the unicode norm table

--- scripts/gen-unicode-norm-table.py
+++ scripts/gen-unicode-norm-table.py
@@ -3,7 +3,7 @@
 import urllib.request
 import os
 
-URL = 'https://www.unicode.org/Public/14.0.0/ucd/UnicodeData.txt'
+URL = 'https://www.unicode.org/Public/15.0.0/ucd/UnicodeData.txt'
 FILE_NAME = 'UnicodeData.txt'
 
 
@@ -22,7 +22,7 @@ print('//!')
 print('//! The current implementation is not the fastest one. Just good enough.')
 print()
 print('#[allow(dead_code)]')
-print('pub const UNICODE_VERSION: (u8, u8, u8) = (14, 0, 0);')
+print('pub const UNICODE_VERSION: (u8, u8, u8) = (15, 0, 0);')
 print()
 print('// Rust support `Option<char>` layout optimization, so it will take only 4 bytes.')
 print('pub const DECOMPOSITION_TABLE: &[(char, char, Option<char>)] = &[')
