95cda71265e6030f20fec70b920777380d90050e
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-06-30 12:01:11 +0200

    [ot-tags] Remove obsolete overrides

--- scripts/gen-tag-table.py
+++ scripts/gen-tag-table.py
@@ -710,10 +710,6 @@ ot.add_language('hy-arevmda', 'HYE')
 ot.remove_language_ot('KGE')
 ot.add_language('und-Geok', 'KGE')
 
-bcp_47.macrolanguages['id'] = {'in'}
-
-bcp_47.macrolanguages['ijo'] = {'ijc'}
-
 ot.add_language('kht', 'KHN')
 ot.names['KHN'] = ot.names['KHT'] + '(Microsoft fonts)'
 ot.ranks['KHN'] = ot.ranks['KHT'] + 1
@@ -804,8 +800,6 @@ ot.add_language('lzh', 'ZHT')
 ot.add_language('lzh-Hans', 'ZHS')
 ot.add_language('yue', 'ZHH')
 ot.add_language('yue-Hans', 'ZHS')
-
-bcp_47.macrolanguages['zom'] = {'yos'}
 
 
 def rank_delta(bcp_47, ot):
