82929f83e3238d36b7f3673bae9a89ea3da8713c
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2024-07-06 22:47:09 +0300

    random fixes + added failing ruqaa font test.

--- src/hb/shape_wasm.rs
+++ src/hb/shape_wasm.rs
@@ -101,7 +101,8 @@ pub(crate) fn shape_with_wasm(
         .get_typed_func::<(u32, u32, u32, u32, u32), i32>(&mut store, "shape")
         .ok()?;
 
-    if let Ok(0) | Err(_) = shape.call(&mut store, (0, 0, 0, 0, 0)) {
+    if let e @ Ok(0) | e @ Err(_) = shape.call(&mut store, (0, 0, 0, 0, 0)) {
+        std::eprintln!("{e:?}");
         return None;
     };
 
@@ -418,7 +419,9 @@ fn buffer_set_contents(mut caller: Caller<'_, ShapingData>, _buffer: u32, cbuffe
 fn debugprint(mut caller: Caller<'_, ShapingData>, s: u32) {
     let memory = caller.get_export("memory").unwrap().into_memory().unwrap();
 
-    // seems reasonable?
+    if memory.data(&caller).get(s as usize).is_none() {
+        return;
+    }
     let bytes = &memory.data(&caller)[s as usize..];
     let msg = CStr::from_bytes_until_nul(bytes)
         .unwrap_or_default()
@@ -523,7 +526,7 @@ impl ttf_parser::OutlineBuilder for GlyphOutline {
 
     fn close(&mut self) {
         // maybe?
-        self.contours.push(self.points.len() as u32)
+        self.contours.push(self.points.len() as u32 - 1)
     }
 }
 
@@ -566,12 +569,24 @@ unsafe impl bytemuck::Pod for CBufferContents {}
 
 #[cfg(test)]
 mod tests {
-    use std::print;
-
     use super::*;
+    use crate::hb::ot_shape::hb_ot_shape_planner_t;
+
+    // helper function
+    fn get_plan_from_buffer(face: &hb_font_t, buffer: &mut UnicodeBuffer) -> hb_ot_shape_plan_t {
+        buffer.guess_segment_properties();
+
+        hb_ot_shape_planner_t::new(
+            &face,
+            buffer.direction(),
+            Some(buffer.script()),
+            buffer.language().as_ref(),
+        )
+        .compile(&[])
+    }
 
     #[test]
-    fn name() -> Result<(), ()> {
+    fn calculator() -> Result<(), ()> {
         // here are the functions needed for this font:
         //
         // (import "env" "buffer_copy_contents" (func (;0;) (type 0)))
@@ -579,8 +594,6 @@ mod tests {
         // (import "env" "font_get_glyph" (func (;2;) (type 3)))
         // (import "env" "font_get_glyph_h_advance" (func (;3;) (type 0)))
         // (import "env" "debugprint" (func (;4;) (type 2)))
-        // (export "memory" (memory 0))
-        // (export "shape" (func 50))
 
         let calculator_font =
             include_bytes!("../../tests/fonts/text-rendering-tests/Calculator-Regular.ttf");
@@ -589,16 +602,87 @@ mod tests {
         let mut buffer = UnicodeBuffer::new();
         buffer.push_str("22/7=");
 
-        let plan = hb_ot_shape_plan_t::new(&face, crate::Direction::LeftToRight, None, None, &[]);
+        let plan = get_plan_from_buffer(&face, &mut buffer);
 
-        let res = shape_with_wasm(&face, &plan, buffer).unwrap();
+        let res = shape_with_wasm(&face, &plan, buffer)
+            .unwrap()
+            .glyph_infos()
+            .iter()
+            .map(|i| i.glyph_id)
+            .collect::<alloc::vec::Vec<_>>();
 
-        // print!("{:?}", res.glyph_infos());
-        for info in res.glyph_infos() {
-            let c = face.glyph_name(info.as_glyph()).unwrap();
-            std::println!("{c}");
-        }
+        // glyphids for 3.142857
+        let expected = alloc::vec![20, 15, 18, 21, 19, 25, 22, 24];
 
+        assert_eq!(expected, res);
+        Ok(())
+    }
+
+    #[test]
+    fn ruqaa() -> Result<(), ()> {
+        // here are the functions needed for this font:
+        //
+        // (import "env" "buffer_copy_contents" (func (;0;) (type 0)))
+        // (import "env" "buffer_set_contents" (func (;1;) (type 0)))
+        // (import "env" "shape_with" (func (;2;) (type 11)))
+        // (import "env" "font_get_face" (func (;3;) (type 4)))
+        // (import "env" "font_glyph_to_string" (func (;4;) (type 7)))
+        // (import "env" "font_get_scale" (func (;5;) (type 3)))
+        // (import "env" "font_copy_glyph_outline" (func (;6;) (type 1)))
+        // (import "env" "debugprint" (func (;7;) (type 5)))
+        // (import "env" "face_get_upem" (func (;8;) (type 4)))
+
+        let ruqaa_font =
+            include_bytes!("../../tests/fonts/text-rendering-tests/ArefRuqaa-Wasm.ttf");
+        let face = hb_font_t::from_slice(ruqaa_font, 0).unwrap();
+
+        let mut buffer = UnicodeBuffer::new();
+        buffer.push_str("أفشوا السلام بينكم");
+
+        let plan = get_plan_from_buffer(&face, &mut buffer);
+
+        let res = shape_with_wasm(&face, &plan, buffer).expect("No shape_with_wasm_result"); // currently failing
+        let res = res
+            .glyph_positions()
+            .iter()
+            .zip(res.glyph_infos().iter())
+            .map(|(p, i)| {
+                (
+                    (p.x_advance, p.x_offset, p.y_offset),
+                    (i.cluster, i.glyph_id),
+                )
+            })
+            .collect::<alloc::vec::Vec<_>>();
+
+        // Gotten using Wasm FontGoggles.
+        let expected = alloc::vec![
+            ((303, 0, -213), (17, 301)),
+            ((321, 0, 20), (16, 243)),
+            ((0, 215, 394), (15, 491)),
+            ((198, 0, 20), (15, 14)),
+            ((0, 167, -81), (14, 494)),
+            ((229, 0, 42), (14, 20)),
+            ((0, 163, 77), (13, 492)),
+            ((313, 0, 213), (13, 30)),
+            ((146, 0, 0), (12, 455)),
+            ((287, 0, 0), (11, 300)),
+            ((-27, 0, -35), (10, 5)),
+            ((732, 0, -35), (9, 275)),
+            ((387, 0, -35), (8, 89)),
+            ((358, 0, 35), (7, 286)),
+            ((248, 0, 0), (6, 3)),
+            ((146, 0, 0), (5, 455)),
+            ((145, 0, 0), (4, 3)),
+            ((280, -146, -164), (3, 388)),
+            ((0, 338, 526), (2, 496)),
+            ((387, 0, 95), (2, 89)),
+            ((0, 259, 807), (1, 491)),
+            ((414, 0, 165), (1, 215)),
+            ((0, 121, 791), (0, 501)),
+            ((248, 0, 0), (0, 3)),
+        ];
+
+        assert_eq!(expected, res);
         Ok(())
     }
 }
